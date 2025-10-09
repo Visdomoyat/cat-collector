1763eb2bbeeb125e571fe281659c83e7ec3d5558
visdomoyat <wisdomoyatokun@gmail.com>
2025-10-09 00:34:01 -0400

    initiating virtual environment & the views pattern

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+#Import HttpResponse to send text-base responses
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return HttpResponse("<h1>Hello Django</h1>")
