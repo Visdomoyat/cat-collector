fefce86ae10b10ab7820ef7c8364562af668d76a
visdomoyat <wisdomoyatokun@gmail.com>
2025-10-15 15:41:25 -0400

    Refactor home view to render home.html template instead of returning raw HTML; update binary files in __pycache__

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 #Import HttpResponse to send text-base responses
-from django.http import HttpResponse
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello Django</h1>")
+    return render(request, "home.html")
 def about(request):
     return render(request, 'about.html')
 
